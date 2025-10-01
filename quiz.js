1c1f95d7e48aa26337fc4501b795293b3e36086b
EsohwodeEmmanuelNyerovwo <emmasonnyerovwo@gmail.com>
2025-10-01 12:49:15 -0700

    Basic Quiz Application

---
+++ quiz.js
@@ -0,0 +1,17 @@
+function checkAnswer() {
+    const correctAnswer = '4';
+    const userAnswer = document.querySelector("[name='quiz']:checked").value;
+
+    const feedback = document.getElementById('feedback');
+
+    if (userAnswer == correctAnswer) {
+        feedback.textContent = 'Correct! Well done.';
+    }
+    if (userAnswer != correctAnswer) {
+        feedback.textContent = 'That\'s incorrect. Try again!';
+    }
+}
+
+const submitAnswer = document.getElementById('submit-answer');
+
+submitAnswer.addEventListener('click', checkAnswer);
